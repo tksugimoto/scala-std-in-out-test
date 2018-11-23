26f3ad352c2863e732427fe1c30ba3a7ec5f2621
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-11-23 16:06:13 +0900

    feat: debug用出力関数を作成
    println でdebug出力すると結果のチェック対象になってしまうため
    System.err.printlnを使い、チェック対象外である標準エラーに出力

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,11 @@ object Main extends App {
   val n = readInt()
   for(_ <- 1 to n) {
     val a = readInt()
+    debug(s"input: $a")
     println(s"${a * 2}")
+  }
+
+  private def debug(msg: Any): Unit = {
+    System.err.println(msg)
   }
 }
